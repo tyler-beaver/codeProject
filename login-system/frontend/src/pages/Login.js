50e32341b63114557f936c7a3a7c9836cfbc1c6e
tbeaver23 <tbeaver23@yahoo.com>
2026-02-05 14:17:47 -0500

    update

--- login-system/frontend/src/pages/Login.js
+++ login-system/frontend/src/pages/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import ForgotPassword from "../components/ForgotPassword";
 import { supabase } from "../supabaseClient";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 function Login({ setToken }) {
--- login-system/frontend/src/pages/Register.js
+++ login-system/frontend/src/pages/Register.js
@@ -2,6 +2,7 @@
  * Registration form for the React frontend
  */
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 import { useNavigate } from "react-router-dom";
