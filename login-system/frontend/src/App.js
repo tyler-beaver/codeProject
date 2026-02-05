2e6485a6710120895d6e1366c6f9a731c606e439
tbeaver23 <tbeaver23@yahoo.com>
2026-02-05 09:52:42 -0500

    update

--- login-system/frontend/src/App.js
+++ login-system/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
