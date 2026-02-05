82d3349bbd0eb27fc0da948c75e79b184c24bc61
tbeaver23 <tbeaver23@yahoo.com>
2026-02-05 13:42:04 -0500

    update

--- login-system/backend/db.js
+++ login-system/backend/db.js
@@ -12,11 +12,14 @@
 // db.js
 const { Pool } = require("pg");
 
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
